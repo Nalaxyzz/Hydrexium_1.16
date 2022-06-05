12f29db5fea97071e897bfff6098d6aab2fbca58
nalaxyzz <milcentenzo@gmail.com>
2022-06-05 23:03:32 +0200

    Version 1.0.0

---
+++ src/main/java/net/mcreator/hydrexium/itemgroup/HydrexiumtabItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.hydrexium.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.hydrexium.item.LingotHydrexiumItem;
+import net.mcreator.hydrexium.Hydrexium116ModElements;
+
+@Hydrexium116ModElements.ModElement.Tag
+public class HydrexiumtabItemGroup extends Hydrexium116ModElements.ModElement {
+	public HydrexiumtabItemGroup(Hydrexium116ModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabhydrexiumtab") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(LingotHydrexiumItem.block, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}
