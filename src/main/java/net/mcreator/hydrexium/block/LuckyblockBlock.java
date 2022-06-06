5636b59134e350b0ecceea093e559982901d25de
nalaxyzz <milcentenzo@gmail.com>
2022-06-06 20:28:39 +0200

    Version 1.0.6 [1.16.5]

--- src/main/java/net/mcreator/hydrexium/block/LuckyblockBlock.java
+++ src/main/java/net/mcreator/hydrexium/block/LuckyblockBlock.java
@@ -2,20 +2,28 @@
 package net.mcreator.hydrexium.block;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
 
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
+import net.mcreator.hydrexium.procedures.LuckyblockcasseProcedure;
 import net.mcreator.hydrexium.itemgroup.HydrexiumtabItemGroup;
 import net.mcreator.hydrexium.Hydrexium116ModElements;
 
+import java.util.Map;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Collections;
 
 @Hydrexium116ModElements.ModElement.Tag
@@ -34,7 +42,8 @@ public class LuckyblockBlock extends Hydrexium116ModElements.ModElement {
 	}
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
 			setRegistryName("luckyblock");
 		}
 
@@ -43,7 +52,24 @@ public class LuckyblockBlock extends Hydrexium116ModElements.ModElement {
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
-			return Collections.singletonList(new ItemStack(this, 1));
+			return Collections.singletonList(new ItemStack(this, 0));
+		}
+
+		@Override
+		public boolean removedByPlayer(BlockState state, World world, BlockPos pos, PlayerEntity entity, boolean willHarvest, FluidState fluid) {
+			boolean retval = super.removedByPlayer(state, world, pos, entity, willHarvest, fluid);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				LuckyblockcasseProcedure.executeProcedure($_dependencies);
+			}
+			return retval;
 		}
 	}
 }
--- src/main/java/net/mcreator/hydrexium/procedures/LuckyblockcasseProcedure.java
+++ src/main/java/net/mcreator/hydrexium/procedures/LuckyblockcasseProcedure.java
@@ -1,6 +1,29 @@
 package net.mcreator.hydrexium.procedures;
 
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.hydrexium.item.LingotdevoidiumItem;
+import net.mcreator.hydrexium.item.LingotdeSlavicniumItem;
+import net.mcreator.hydrexium.item.LingotdeBluteniumItem;
+import net.mcreator.hydrexium.item.LingotHydrexiumItem;
+import net.mcreator.hydrexium.block.LuckyblockBlock;
+import net.mcreator.hydrexium.block.InferniumblocBlock;
+import net.mcreator.hydrexium.block.HydrexiumblocBlock;
 import net.mcreator.hydrexium.Hydrexium116ModElements;
+import net.mcreator.hydrexium.Hydrexium116Mod;
 
 import java.util.Map;
 
@@ -11,6 +34,79 @@ public class LuckyblockcasseProcedure extends Hydrexium116ModElements.ModElement
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				Hydrexium116Mod.LOGGER.warn("Failed to load dependency x for procedure Luckyblockcasse!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				Hydrexium116Mod.LOGGER.warn("Failed to load dependency y for procedure Luckyblockcasse!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				Hydrexium116Mod.LOGGER.warn("Failed to load dependency z for procedure Luckyblockcasse!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				Hydrexium116Mod.LOGGER.warn("Failed to load dependency world for procedure Luckyblockcasse!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
 		double LuckyBlock = 0;
+		LuckyBlock = (double) Math.random();
+		if (((LuckyBlock) < 0)) {
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(LuckyblockBlock.block, (int) (1)));
+				entityToSpawn.setPickupDelay((int) 10);
+				entityToSpawn.setNoDespawn();
+				world.addEntity(entityToSpawn);
+			}
+		} else if (((LuckyBlock) < 0)) {
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(LingotHydrexiumItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay((int) 10);
+				entityToSpawn.setNoDespawn();
+				world.addEntity(entityToSpawn);
+			}
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(LingotdeSlavicniumItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay((int) 10);
+				entityToSpawn.setNoDespawn();
+				world.addEntity(entityToSpawn);
+			}
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(LingotdevoidiumItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay((int) 10);
+				entityToSpawn.setNoDespawn();
+				world.addEntity(entityToSpawn);
+			}
+			if (world instanceof World && !world.isRemote()) {
+				ItemEntity entityToSpawn = new ItemEntity((World) world, x, y, z, new ItemStack(LingotdeBluteniumItem.block, (int) (1)));
+				entityToSpawn.setPickupDelay((int) 10);
+				entityToSpawn.setNoDespawn();
+				world.addEntity(entityToSpawn);
+			}
+		} else if (((LuckyBlock) < 0.1)) {
+			world.setBlockState(new BlockPos((int) x, (int) (y + 0), (int) z), HydrexiumblocBlock.block.getDefaultState(), 3);
+			world.setBlockState(new BlockPos((int) x, (int) (y + 0), (int) z), LuckyblockBlock.block.getDefaultState(), 3);
+			world.setBlockState(new BlockPos((int) x, (int) (y + 2), (int) z), Blocks.POLISHED_DIORITE.getDefaultState(), 3);
+			world.setBlockState(new BlockPos((int) x, (int) (y + 3), (int) z), Blocks.COBBLESTONE.getDefaultState(), 3);
+			world.setBlockState(new BlockPos((int) x, (int) (y + 4), (int) z), Blocks.CRYING_OBSIDIAN.getDefaultState(), 3);
+			world.setBlockState(new BlockPos((int) x, (int) (y + 5), (int) z), InferniumblocBlock.block.getDefaultState(), 3);
+			if (world instanceof ServerWorld) {
+				Entity entityToSpawn = new PigEntity(EntityType.PIG, (World) world);
+				entityToSpawn.setLocationAndAngles(x, (y + 6), z, world.getRandom().nextFloat() * 360F, 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn((ServerWorld) world, world.getDifficultyForLocation(entityToSpawn.getPosition()),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+		}
 	}
 }
